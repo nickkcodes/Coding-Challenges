84b9e2ade829c693472c32c6991585ac1d0338c2
Nick <mayumicalamno5@gmail.com>
2026-04-21 21:24:22 +0800

    Initial commit: Add camelCase converter solution

---
+++ underscore.py
@@ -0,0 +1,8 @@
+def to_camel_case(text):
+    words = text.replace("-", "_").split("_")
+
+    return words[0] + "".join(word.capitalize() for word in words[1:])
+
+print(to_camel_case("the-stealth-warrior"))
+print(to_camel_case("The_Stealth_Warrior"))
+print(to_camel_case("The_Stealth-Warrior"))
