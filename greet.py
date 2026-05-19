482750e33728a17409adf8c0b2628f20d475134c
Nick <mayumicalamno5@gmail.com>
2026-05-19 20:00:18 +0800

    code wars greet

---
+++ greet.py
@@ -0,0 +1,9 @@
+def greet(name,owner):
+
+    if name == owner:
+        return 'Hello boss'
+    else:
+        return 'Hello guest'
+    
+print(greet('Daniel','Daniel'))
+print(greet('Greg','Daniel'))
